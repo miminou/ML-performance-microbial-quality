74abf3766c7bfbc13ea8a61d8ee126b11fba4152
manel naloufi <63862628+naloufi-manel@users.noreply.github.com>
2021-07-19 11:44:52 +0200

    Update ML-based_prediction_comparison.R

--- ML-based_prediction_comparison.R
+++ ML-based_prediction_comparison.R
@@ -3,6 +3,15 @@
 # version :  V3.5.1
 #########
 
+# Visualization of the data distribution of our dataset 
+smv <- read.csv("~/Bureau/BD/donnee+pluvio/smv-complet/donnee_brute_sans_na/smv.csv")
+par(mfrow=c(2,5))
+for (i in 5: length(smv)){
+  boxplot(smv[,i], col="grey", ylab=colnames(smv)[i])
+}
+boxplot(log(smv$Ecoli), col="grey", ylab="Log (E.coli)")
+
+
 # Plot visualization of the performance results (RMSE, MAE and RPD) of the machine learning models
 par(mfrow=c(1,3))
 plot_performance6 <- read_excel("plot-performance6.xlsx", sheet = "rmse", 
